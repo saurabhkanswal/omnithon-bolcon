4c3eab65fe3ce017d89d122c6c40944971e1f210
saurabhkanswal <saurabhkanswal8101@gmail.com>
2019-11-16 13:14:03 +0530

    updating contract

---
+++ contracts/campaign.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.19;
+
+contract Campaign {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
