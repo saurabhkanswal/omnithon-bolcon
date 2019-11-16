6d34f041243864a32147445b5e2a31ecda91b646
Akash190301 <46964720+Akash190301@users.noreply.github.com>
2019-11-16 19:11:49 +0530

    updation
    of factory instance

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,6 +3,8 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    ''
+    '0x3663DfBCCda81C92bC82DEe488eA524BD41aEa16'
 
 );
+
+export default instance;
