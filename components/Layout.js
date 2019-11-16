c6742b70ad4723a579c1b7c97defca00b180ddc1
Akash190301 <46964720+Akash190301@users.noreply.github.com>
2019-11-17 00:35:47 +0530

    till 00.35
    header files

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+export  default (props) => {
+    return (
+        <div>
+            <Header />
+            {props.children}
+
+            <h1>Im a footer</h1>
+        </div>
+    );
+};
