56b7222d329d8321232d800e60f8bd78b24c3493
saurabhkanswal <saurabhkanswal8101@gmail.com>
2019-11-17 04:40:35 +0530

    update route.js and change in file structure

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import {Form, Button, Input, Message} from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import {Router } from '../../routes';
 class CampaignNew extends Component{
     state={
         minimumContribution:'',
@@ -19,6 +20,8 @@ try{
         await factory.methods.createCampaign(this.state.minimumContribution).send({
         from :accounts[0]
         });
+        Router.pushRoute('/');
+
     } catch(err){
         this.setState({errorMessage: err.message})
     }
