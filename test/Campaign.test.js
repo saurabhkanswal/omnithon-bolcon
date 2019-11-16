5ee3c5d28b0c224e408c81a838679aba2442c733
Akash190301 <46964720+Akash190301@users.noreply.github.com>
2019-11-16 17:42:06 +0530

    #3
    jjkjh

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -73,3 +73,10 @@ describe('Campaigns', () => {
 
     assert.equal('Buy batteries', request.description);
   });
+<<<<<<< HEAD
+
+});
+=======
+});
+
+>>>>>>> e515665cc38b6efa8630946b9b20b5b65d7007c9
