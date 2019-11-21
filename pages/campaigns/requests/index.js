6f1966f5d40ce24fc0bf2a858960f3b3259cd449
saurabhkanswal <saurabhkanswal8101@gmail.com>
2019-11-22 01:17:45 +0530

    up

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,29 @@
 import React, { Component} from 'react';
+import { Button } from 'semantic-ui-react';
+import {Link} from '../../../routes';
+import Layout from '../../../components/Layout';
 class RequestIndex extends Component {
-    render(){
-        return <h3>Request list</h3>
-       
+        static async getInitialProps(props){
+            const {address} = props.query;
+            return {address};
+        }
+    
+    render() {
+
+        return(
+            <Layout>
+            <h3>Request list</h3>
+            <Link route={`/campaigns/${this.props.address}/requests/new`}>
+            <a>
+            <Button primary>
+            Add Request!
+            </Button>
+            </a>
+            </Link>
+            </Layout>
+        )
     }
 }
+
 
 export default RequestIndex;
