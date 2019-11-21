be283ee86cb0337fb6005b97a4f682a175054f14
saurabhkanswal <saurabhkanswal8101@gmail.com>
2019-11-22 00:23:40 +0530

    up

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+class RequestIndex extends Component {
+    render(){
+        return <h3>Request list</h3>
+       
+    }
+}
+
+export default RequestIndex;
