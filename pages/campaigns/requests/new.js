a94e893892fa2268f5ad01b243d51e7206e8e968
saurabhkanswal <saurabhkanswal8101@gmail.com>
2019-11-22 12:08:58 +0530

    up

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,9 +1,66 @@
-const routes = require('next-routes')();
+import React, { Component} from 'react';
+import {Form, Button, Message, Input} from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import {Link, Router} from '../../../routes';
+import Layout from '../../../components/Layout';
 
-routes
-    .add('/campaigns/new','/campaigns/new')
-    .add('/campaigns/:address', '/campaigns/show')
-    .add('/campaigns/:address/requests','/campaigns/requests/index')
-    .add('/campaigns/:address/requests/new','/campaigns/requests/new');
+class RequestNew extends Component {
+    state = {
+        value: '',
+        description: '',
+        recipient: ''
+    }
+    static async getInitialProps(props){
+        const {address} = props.query;
+        return { address}
+    }
+    onSubmit = async event => {
+        event.preventDefault();
+        const campaign = Campaign(this.props.address);
+        const { description, value, recipient} = this.state;
+        try{
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.createRequest(
+                description,
+                web3.utils.toWei(value, 'ether'),
+                recipient
+            ).send({ from: accounts[0]})
+        }catch(err){
 
-module.exports = routes;
+        }
+    }
+
+    render() {
+        return (
+            <Layout>
+            <h3>Create a Request</h3>
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                <label>Description</label>
+                <Input
+                value={this.state.description}
+                onChange={event => this.setState({description: event.target.value})}
+                />
+                </Form.Field>
+                <Form.Field>
+                <label>Value in ether</label>
+                <Input
+                value={this.state.value}
+                onChange= {event => this.setState({value: event.target.value})}
+                />
+                </Form.Field>
+                <Form.Field>
+                <label>Recipient</label>
+                <Input
+                value= {this.state.recipient}
+                onChange={event => this.setState({recipient: event.target.value})}
+                />
+                </Form.Field>
+                <Button primary>Create!</Button>
+            </Form>
+            </Layout>
+        )
+    }
+}
+export default RequestNew;
